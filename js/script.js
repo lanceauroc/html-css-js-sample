fb7aeb2f55c30185960d4be37cdfdb1600f151c0
Lance <lance.auro.c@gmail.com>
2024-05-02 22:11:28 +0800

    implemented hamburger-like responsive menu via toggle

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 let navbar = document.querySelector('nav');
+let menuLinks = document.getElementById('menu-links');
+
+function toggleMenu () {
+    menuLinks.classList.toggle('show-menu'); /* when triggered, show-menu is added to the element by the specified id*/
+}
 
 window.onscroll = function() {
     if(window.scrollY > 0) { // identifies that the page has been scrolled
